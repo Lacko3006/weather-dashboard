a6775e851b7969f1307e2f4b5fda55d186be4232
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-01 18:10:29 +0000

    added openweather API and linked to HTML h1

--- assests/script.js
+++ assests/script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const weatherDashboardElement = document.querySelector("#weather-dashboard")
+
+let weatherBoardUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=d55f24fccb2e40d4eec3016c984da28e';
+
+function getApi(requestUrl) {
+  fetch(weatherBoardUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      weatherDashboardElement.textContent = data.city.sunrise
+    });
+}
+
+getApi(weatherBoardUrl)
