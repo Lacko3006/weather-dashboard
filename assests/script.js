e6e8b39849d4e09929875ae0ecbc2fdc813b05d2
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-01 18:35:19 +0000

    connected geocode api to html using textContent

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,7 @@
 const weatherDashboardElement = document.querySelector("#weather-dashboard")
 
+let geocodingUrl = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d55f24fccb2e40d4eec3016c984da28e";
+
 let weatherBoardUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=d55f24fccb2e40d4eec3016c984da28e';
 
 function getApi(requestUrl) {
@@ -10,8 +12,22 @@ function getApi(requestUrl) {
     })
     .then(function (data) {
       console.log(data);
-      weatherDashboardElement.textContent = data.city.sunrise
+    //   weatherDashboardElement.textContent = data.city.sunrise
     });
 }
 
 getApi(weatherBoardUrl)
+
+function getGeocodingUrl(requestUrl) {
+    fetch(geocodingUrl)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        weatherDashboardElement.textContent = data[0].name
+      });
+  }
+  
+  getGeocodingUrl(geocodingUrl)
