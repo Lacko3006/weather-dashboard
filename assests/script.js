6f739a12d4be126cf712562bc75488c5adeb4d18
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-03 17:42:04 +0000

    added search the API url

--- assests/script.js
+++ assests/script.js
@@ -5,37 +5,17 @@ let searchButton = document.querySelector("#search-btn");
 searchButton.addEventListener("click", weatherSearch);
 
 function weatherSearch(){
-  
-}
-
+let weatherBoardUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"&appid=d55f24fccb2e40d4eec3016c984da28e";
 function getApi(requestUrl) {
   fetch(weatherBoardUrl)
     .then(function (response) {
-      console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      //   weatherDashboardElement.textContent = data.city.sunrise
+      console.log(data.list[0].wind.speed);
     });
 }
 
 getApi(weatherBoardUrl);
-
-function getGeocodingUrl(geocodingUrl) {
-  fetch(geocodingUrl)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      weatherDashboardElement.textContent = data[0].lat;
-    });
 }
 
-getGeocodingUrl(geocodingUrl);
-
-// let geocodingUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" //input.value "appid=d55f24fccb2e40d4eec3016c984da28e";
-
-// let weatherBoardUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=d55f24fccb2e40d4eec3016c984da28e';
