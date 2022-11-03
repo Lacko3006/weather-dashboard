841f513a1a7f3c27874e1cf01658c1379919d120
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-03 18:51:31 +0000

    wind added to cards

--- assests/script.js
+++ assests/script.js
@@ -1,21 +1,37 @@
 const weatherDashboardElement = document.querySelector("#weather-dashboard");
 const inputValue = document.querySelector("#data-input");
-let searchButton = document.querySelector("#search-btn");
+const searchButton = document.querySelector("#search-btn");
+const dataCardsElement = document.querySelectorAll(".card")
+const windElement = document.querySelectorAll(".wind")
 
 searchButton.addEventListener("click", weatherSearch);
 
 function weatherSearch(){
 let weatherBoardUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"&appid=d55f24fccb2e40d4eec3016c984da28e";
-function getApi(requestUrl) {
+//function getApi(requestUrl) {
   fetch(weatherBoardUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.list[0].wind.speed);
-    });
+      console.log(data)
+
+    let dataStored = data.list
+      let j=0
+      for (i=0; i < dataStored.length; i=i+8){
+ 
+      windElement[j].textContent = data.list[i].wind.speed
+      console.log(data.list[i].wind.speed)
+      j=j+1
+}
 }
 
-getApi(weatherBoardUrl);
+
+
+);
 }
+
+  
+
+//getApi(weatherBoardUrl);}
 
