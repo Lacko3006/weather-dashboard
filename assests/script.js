3dc54c8f3a774d1a92d305265f696a0bc55ab575
Sam Laxton <samlaxton@sams-air-2.home>
2022-11-05 19:16:21 +0000

    added city name

--- assests/script.js
+++ assests/script.js
@@ -1,37 +1,38 @@
 const weatherDashboardElement = document.querySelector("#weather-dashboard");
 const inputValue = document.querySelector("#data-input");
 const searchButton = document.querySelector("#search-btn");
-const dataCardsElement = document.querySelectorAll(".card")
-const windElement = document.querySelectorAll(".wind")
+const saveSearchElement = document.querySelector(".saved-search");
+const dataCardsElement = document.querySelectorAll(".card");
+const cityNameElement = document.querySelectorAll(".name");
+const windElement = document.querySelectorAll(".wind");
+
 
 searchButton.addEventListener("click", weatherSearch);
 
-function weatherSearch(){
-let weatherBoardUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"&appid=d55f24fccb2e40d4eec3016c984da28e";
-//function getApi(requestUrl) {
-  fetch(weatherBoardUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
+function weatherSearch() {
+  let weatherBoardUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    inputValue.value +
+    "&appid=d55f24fccb2e40d4eec3016c984da28e";
+  function getApi(requestUrl) {
+    fetch(weatherBoardUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        let j = 0;
+        let dataStored = data.list;
+        cityNameElement[j].textContent = data.city.name;
+        for (i = 0; i < dataStored.length; i = i + 8) {
+          windElement[j].textContent = data.list[i].wind.speed;
+          
+          console.log(data.list[i].wind.speed);
+          j = j + 1;
+        }
+          saveSearchElement.textContent = data.city.name;
+      });
+  }
 
-    let dataStored = data.list
-      let j=0
-      for (i=0; i < dataStored.length; i=i+8){
- 
-      windElement[j].textContent = data.list[i].wind.speed
-      console.log(data.list[i].wind.speed)
-      j=j+1
+  getApi(weatherBoardUrl);
 }
-}
-
-
-
-);
-}
-
-  
-
-//getApi(weatherBoardUrl);}
-
